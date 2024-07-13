6b0997fe4ec9a9ec2eed99eff85120dfb7a253bc
Maureen-Gatweri <maureennmwendwa@gmail.com>
2024-07-13 22:29:20 +0300

    add podcast section

---
+++ mageziwebsite/src/Podcast/index.js
@@ -0,0 +1,44 @@
+import "./index.css";
+import "@fontsource/libre-baskerville";
+
+const About = () => {
+  return (
+    <div>
+      <div>
+        <h2>Podcast</h2>
+        <hr />
+        <h4> Buds on Air</h4>
+        <img
+          src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQrxpLiQv5w1YDzK1n2MO_GksK910BKepW9mC5ykxRBSksMyyKi"
+          alt="check"
+        />
+      </div>
+      <div>
+        <p>
+          
+          Buds on Air aims to provide valuable insights and practical advice on
+          preventing burnout, navigating career challenges, and exploring the
+          evolving landscape of work.Burnout Prevention: The show will delve
+          into strategies for maintaining work-life balance, managing stress,
+          and prioritizing self-care to help listeners avoid burnout and
+          maintain their well-being. Tune in for this and more interesting
+          episodes coming your way. Super excited to share our experiences.
+        </p>
+
+        <h2> Upcoming episodes</h2>
+        <ul>
+          <li>Strategies for maintaining work-life balance</li>
+          <li>Managing stress</li>
+          <li>prioritizing self care</li>
+          <li>Burnout prevention</li>
+          <li>Women in Tech</li>
+        </ul>
+        <img
+          src="https://i.pinimg.com/474x/34/8b/c1/348bc17323a1ad8dc50ad0ce2aed0861.jpg"
+          alt="check"
+        />
+      </div>
+    </div>
+  );
+};
+export default About;
