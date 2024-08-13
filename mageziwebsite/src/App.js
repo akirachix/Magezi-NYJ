fed35fd502eae6cef916ca2186e628625e12a29d
Valentine-Nkatha <valentinnenkatha@gmail.com>
2024-08-13 16:49:06 +0300

    made changes

--- mageziwebsite/src/App.js
+++ mageziwebsite/src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Blogs from "./Blogs";
+import Blogstwo from "./Blogstwo";
+import CaseStudy from "./CaseStudy";
+import GroupDescription from "./GroupDescription";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Blogs />} />
+          <Route path="/Blogstwo" element={<Blogstwo />} />
+          <Route path="/CaseStudy" element={<CaseStudy/>}/>
+          <Route path="/GroupDescription" element={<GroupDescription/>}/> 
+          <Route path="/" element={<GroupDescription/>}/> 
+          <Route path="/Blogs" element={<Blogs />} />
+          <Route path="/" element={<CaseStudy/>}/>
+          <Route path="/Blogs" element={<Blogs />} />
+          <Route path="/" element={<Blogstwo />} />
+          <Route path="/Blogs" element={<Blogs />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
